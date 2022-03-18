4f40c7f5f5b0403aa62498ec5f136e7996c58214
Mustika Marito Siahaan <mustikasiahaan4@gmail.com>
2022-03-18 19:21:55 +0700

    new

---
+++ 12_Vue CLI/praktikum/SECTION 12 - VUE CLI/section12/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <ol>
+        <li v-for="item in list" :key=item.id>
+          {{ item }}
+        </li>
+      </ol>
+      <input type="text" v-model="visi"/>
+      <button @click="listVisi">Tambahkan</button>
+      <div v-if="list.length >= 4">Hebat!</div>      
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return{
+        visi: '',
+        list: []
+    }
+  },
+    methods: {
+      listVisi(){
+        this.list.push(this.visi)
+        this.visi = ''
+
+      }
+    }
+}
+</script>
+
+
