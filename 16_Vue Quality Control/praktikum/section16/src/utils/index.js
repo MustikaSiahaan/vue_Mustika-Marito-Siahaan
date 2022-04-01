c4e3cb9ae95e895412b8ecddbdace613b0bcfc45
Mustika Marito Siahaan <mustikasiahaan4@gmail.com>
2022-04-02 01:03:56 +0700

    updt

--- 16_Vue Quality Control/praktikum/section16/src/utils/index.js
+++ 16_Vue Quality Control/praktikum/section16/src/utils/index.js
@@ -1,3 +1,15 @@
-export const pemjumlahan = (bilanganSatu, bilanganDua) => {
+export const penjumlahan = (bilanganSatu, bilanganDua) => {
     return bilanganSatu + bilanganDua;
-}
+};
+
+export const pengurangan = (bilanganSatu, bilanganDua) => {
+    return bilanganSatu - bilanganDua;
+};
+
+export const pembagian = (bilanganSatu, bilanganDua) => {
+    return bilanganSatu / bilanganDua;
+};
+
+export const perkalian = (bilanganSatu, bilanganDua) => {
+    return bilanganSatu * bilanganDua;
+};
--- 16_Vue Quality Control/praktikum/section16/tests/unit/utils.spec.js
+++ 16_Vue Quality Control/praktikum/section16/tests/unit/utils.spec.js
@@ -1,8 +1,53 @@
-import { penjumlahan } from '@/utils';
+import { penjumlahan } from '@/utils/index.js';
+import { pengurangan } from '@/utils/index.js';
+import { pembagian } from '../../src/utils';
+import { perkalian} from '../../src/utils';
 
+describe("Melakukan operasi penjumlahan", () => {
+  it("Lakukan penjumlahan bilangan positif", () => {
+    expect(penjumlahan(1, 7)).toBe(8);
+  });
+  it("Penjumlahan bilangan positif dan negatif", () => {
+    expect(penjumlahan(1, -7)).toBe(-6);
+  });
+  it("Penjumlahan bilangan non numerik", () => {
+    expect(penjumlahan("1", "8")).toBe("18");
+  });
+});
 
-describe("Melakukan penjumlahan", () => {
-  test('Penjumlahan dua bilangan positif', () => {
-    expect(penjumlahan(2, 4)).toBe(6);
-  })
-})
+describe("Melakukan operasi pengurangan", () => {
+  it("Lakukan pengurangan bilangan positif", () => {
+    expect(pengurangan(3, 2)).toBe(1);
+  });
+  it("Pengurangan bilangan negatif", () => {
+    expect(penjumlahan(-1, -7)).toBe(-8);
+  });
+  it("Pengurangan bilangan positif dan negatif", () => {
+    expect(pengurangan(1, -7)).toBe(8);
+  });
+});
+
+describe("Melakukan operasi pembagian", () => {
+  it("Lakukan pembagian bilangan positif", () => {
+    expect(pembagian(6, 2)).toBe(3);
+  });
+  it("Pembagian bilangan negatif", () => {
+    expect(pembagian(-4, -2)).toBe(2);
+  });
+  it("Pembagian bilangan positif dan negatif", () => {
+    expect(pembagian(-49, 7)).toBe(-7);
+  });
+});
+
+describe("Melakukan operasi perkalian", () => {
+  it("Lakukan perkalian bilangan positif", () => {
+    expect(perkalian(4, 3)).toBe(12);
+  });
+  it("Perkalian bilangan negatif", () => {
+    expect(perkalian(-4, -2)).toBe(8);
+  });
+  it("Perkalian bilangan positif dan negatif", () => {
+    expect(perkalian(8, -7)).toBe(-56);
+  });
+});
+
